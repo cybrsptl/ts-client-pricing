7d548659caae8d3e269b8d9de3d4c010f7f6249e
Aubrey Falconer <a@aubreyfalconer.com>
2023-02-06 20:54:32 -0800

    IPinfo case standardization

--- constants/CustomPricingData.tsx
+++ constants/CustomPricingData.tsx
@@ -163,7 +163,7 @@ const PricingAccounts = [
 				>
 					<Image
 						src={require("../public/icons/enrichment_partners.svg")}
-						alt={"IpInfo | GreyNoise"}
+						alt={"IPinfo | GreyNoise"}
 					/>
 				</Box>
 			</>
